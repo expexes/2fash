2d7d0402f4def506aa66041eda8cdf26ac5e1f00
Davyd Kohut <expexes@gmail.com>
2019-06-19 00:44:23 +0300

    installer

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ mkdir -p "$FASH_DIRECTORY_ACCOUNTS"
 check_deps
 
 echo "...moving bin to bin"
-mv * .* "$FASH_DIRECTORY_BIN"
+mv . .* "$FASH_DIRECTORY_BIN"
 
 echo "...creating symbolic link in /usr/local/bin"
 sudo ln -s "${FASH_DIRECTORY_BIN}/2fash" "/usr/local/bin/"

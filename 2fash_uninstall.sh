93e16e5925ea10289353a1997667b129efda7a0b
Davyd Kohut <expexes@gmail.com>
2019-06-20 17:55:25 +0300

    fix installer && add uninstaller

---
+++ 2fash_uninstall.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+FASH_DIRECTORY=$1
+
+[[ "$FASH_DIRECTORY" == "" ]] && FASH_DIRECTORY=$HOME/.2fash
+
+echo -en "Are you sure? (y/N): "
+read rusure
+[[ ! ${rusure} =~ [yY]|[yY][eE][sS] ]] && exit 0
+
+rm -rf "${FASH_DIRECTORY}/bin"
+rm -rf "$HOME/.config/fish/functions/2fash.fish"
+rm -rf "$HOME/.config/fish/completions/2fash.fish"
+
+echo -en "Delete all accounts? (y/N): "
+read delallacc
+[[ ! ${delallacc} =~ [yY]|[yY][eE][sS] ]] && exit 0
+
+rm -rf "${FASH_DIRECTORY}/accounts"
+rm -rf "${FASH_DIRECTORY}"
--- install.sh
+++ install.sh
@@ -13,14 +13,14 @@ mkdir -p "$FASH_DIRECTORY"
 mkdir -p "$FASH_DIRECTORY_ACCOUNTS"
 
 install_bin() {
-	echo " ...Coping files to ~/.2fash/bin"
+	echo " ...Coping files to $HOME/.2fash/bin"
 	cp -a . "$FASH_DIRECTORY_BIN"
 }
 
 install_fash_bash() {
 	echo " ...Installing for bash"
-	[[ $(grep -i "export FASH_DIRECTORY" "$HOME/.bashrc") ]] || echo ""
-	[[ $(grep -i "export FASH_DIRECTORY" "$HOME/.bashrc") ]] || echo "export FASH_DIRECTORY='$HOME/.2fash'" >> ~/.bashrc
+	[[ $(grep -i "export FASH_DIRECTORY" "$HOME/.bashrc") ]] || echo "" >> "$HOME/.bashrc"
+	[[ $(grep -i "export FASH_DIRECTORY" "$HOME/.bashrc") ]] || echo "export FASH_DIRECTORY='$HOME/.2fash'" >> "$HOME/.bashrc"
 	[[ $(grep -i 'source "$FASH_DIRECTORY/bin/bash/bash-init.sh"' "$HOME/.bashrc") ]] || echo '[[ -s "$FASH_DIRECTORY/bin/bash/bash-init.sh" ]] && source "$FASH_DIRECTORY/bin/bash/bash-init.sh"' >> "$HOME/.bashrc"
 }
 
@@ -33,6 +33,8 @@ install_fash_fish() {
 }
 
 install_fash() {
+	echo ""
+
 	if [[ -d "$FASH_DIRECTORY_BIN" ]]; then
 		echo -en "You already have 2fash installed. Do you really want to install again? (y/N): "
 		read really < /dev/tty
@@ -47,6 +49,9 @@ install_fash() {
 	install_bin
 	install_fash_bash
 	install_fash_fish
+
+	echo ""
+	echo "Done"
 }
 
 check_deps

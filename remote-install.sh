e0fbda875b477db48434cba50e163f5a42488828
Davyd Kohut <expexes@gmail.com>
2019-07-17 13:37:20 +0300

    1.0.1

--- remote-install.sh
+++ remote-install.sh
@@ -7,9 +7,11 @@ GIT_CLONE_DESTINATION="/tmp/2fash-git-$(date | sed 's/ /_/g')"
 
 # -------------------------------------------------------
 
-if [[ -z $1 ]]; then
-	git clone -b $1 "$GIT_CLONE_REPO" "$GIT_CLONE_DESTINATION" --single-branch && cd "$GIT_CLONE_DESTINATION" && basb install.sh
+if ! [[ -z "$1" ]]; then
+	echo "  from $1"
+	git clone -b "$1" "$GIT_CLONE_REPO" "$GIT_CLONE_DESTINATION" --single-branch && cd "$GIT_CLONE_DESTINATION" && bash install.sh
 else
+	echo "  from master"
 	git clone "$GIT_CLONE_REPO" "$GIT_CLONE_DESTINATION" && cd "$GIT_CLONE_DESTINATION" && bash install.sh
 fi
 
